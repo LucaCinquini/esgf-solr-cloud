cf20133d7f207625206dc073741a717eb23d8990
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-08-20 13:37:55 -0600

    Using --max 1000

--- scripts/solr_harvest.sh
+++ scripts/solr_harvest.sh
@@ -10,5 +10,5 @@ cd /usr/local/esgfpy-publish
 # loop over collections
 for collection in "datasets" "files" "aggregations"
 do
-  python esgfpy/migrate/solr2solr.py --core ${collection} ${SOLR_SOURCE_URL} ${SOLR_TARGET_URL}
+  python esgfpy/migrate/solr2solr.py --core ${collection} ${SOLR_SOURCE_URL} ${SOLR_TARGET_URL} --max 1000
 done
