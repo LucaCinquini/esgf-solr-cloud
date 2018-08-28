75d11e2a00476bff570426e5371f689e21751942
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-08-28 11:19:14 -0600

    Cosmetics

--- scripts/solr_config.sh
+++ scripts/solr_config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Script to initialize Solr collections at startup
+# The following env variables must be set before invoking this script:
+# ZOOKEEPER_HOST, ZOOKEEPER_PORT
+# SOLR_HOST, SOLR_PORT
+# SOLR_NUM_SHARDS, SOLR_NUM_REPLICAS, SOLR_MAX_SHARDS_PER_NODE
 
 set -e
 
@@ -14,7 +18,6 @@ PARENT_DIR="$(dirname $SOURCE_DIR)"
 /opt/solr/bin/solr zk upconfig -z ${ZOOKEEPER_HOST}:${ZOOKEEPER_PORT} -d ${PARENT_DIR}/solr/solr-home/conf -confname esgf_config
 
 # create collections, if not existing already, using the latest configuration
-# shards="esgf-node.jpl.nasa.gov,esgf-node.llnl.gov,esgf-data.dkrz.de"
 for collection in "datasets" "files" "aggregations"
 do
   # try querying this collection to see if it exists
@@ -22,12 +25,17 @@ do
   	 echo "Collection ${collection} already exists"
   else
   	echo "Creating collection: $collection"
+  	
   	# use default hash algorithm as sharding strategy
     curl "http://${SOLR_HOST}:${SOLR_PORT}/solr/admin/collections?action=CREATE&name=${collection}&numShards=${SOLR_NUM_SHARDS}&replicationFactor=${SOLR_NUM_REPLICAS}&maxShardsPerNode=${SOLR_MAX_SHARDS_PER_NODE}&collection.configName=esgf_config"
+	
 	# use field 'index_node' as sharding strategy
-    #curl "http://${SOLR_HOST}:${SOLR_PORT}/solr/admin/collections?action=CREATE&name=${collection}&numShards=${SOLR_NUM_SHARDS}&replicationFactor=${SOLR_NUM_REPLICAS}&maxShardsPerNode=${SOLR_MAX_SHARDS_PER_NODE}&collection.configName=esgf_config&router.name=implicit&router.field=index_node&shards=${shards}"
+	# shards="esgf-node.jpl.nasa.gov,esgf-node.llnl.gov,esgf-data.dkrz.de"
+    # curl "http://${SOLR_HOST}:${SOLR_PORT}/solr/admin/collections?action=CREATE&name=${collection}&numShards=${SOLR_NUM_SHARDS}&replicationFactor=${SOLR_NUM_REPLICAS}&maxShardsPerNode=${SOLR_MAX_SHARDS_PER_NODE}&collection.configName=esgf_config&router.name=implicit&router.field=index_node&shards=${shards}"
+    
     # disable schemaless feature
     curl "http://${SOLR_HOST}:${SOLR_PORT}/solr/${collection}/config" -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
+  
   fi
 done
 
