35945a61b551c63944c0e59af4bc6fbb32722b20
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-08-28 10:55:16 -0600

    Using 3 replicas per shard.

--- kubernetes/scripts/cleanup.sh
+++ kubernetes/scripts/cleanup.sh
@@ -3,6 +3,7 @@
 # script to delete all Kubernetes resources for the ESGF Solr Cloud cluster
 
 kubectl delete deployment,svc,jobs,ingress,statefulset -l stack=esgf-solr-cloud
+
 kubectl delete job solr-harvester
 
-#kubectl delete pvc -l stack=esgf-solr-cloud
+kubectl delete pvc -l stack=esgf-solr-cloud
